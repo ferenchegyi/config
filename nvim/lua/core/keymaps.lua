03421327c3f037751d021949b4bd38faeb2001f4
Ferenc Hegyi <hegyiferenc@proton.me>
2025-12-25 20:12:35 +0100

    Organize and comment keymaps for future understanding

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -1,38 +1,45 @@
 local opts = { noremap = true, silent = true }
 
+-- Leader
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "moves lines down in visual selection" })
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
+-- Moving code up and down when selected
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move selection down and auto-indent" })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move selection up and auto-indent" })
 
-vim.keymap.set("n", "<C-d>","<C-d>zz", { desc = "move down in buffer with cursor centered" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "move up in buffer with cursor centered" })
+-- Moving up and down and cursor centered
+vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half-page down and center cursor" })
+vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half-page up and center cursor" })
 
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+-- Search navigation with cursor centered
+vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
 
-vim.keymap.set("v", "<", "<gv", opts)
-vim.keymap.set("v", ">", ">gv", opts)
+-- Indentation (remains in visual mode after indenting)
+vim.keymap.set("v", "<", "<gv", { desc = "Indent left and re-select" })
+vim.keymap.set("v", ">", ">gv", { desc = "Indent right and re-select" })
 
-vim.keymap.set("x", "<leader>p", [["_dP]])
-vim.keymap.set("v", "p", '"_dp', opts)
-vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
-vim.keymap.set("i", "<C-c>", "<Esc>")
-vim.keymap.set("n", "<C-c>", ":nohl<CR>", { desc = "Clear search hl", silent = true })
+-- Clipboard management (The "Void Register" patterns)
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste over selection without losing current paste buffer" })
+vim.keymap.set("v", "p", '"_dp', { desc = "Paste without overwriting register" })
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete into void register (don't copy)" })
+vim.keymap.set("n", "x", '"_x', { desc = "Delete character into void register" })
 
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "x", '"_x', opts) -- doesn't copy when deleting a char
+-- Navigation & Utilities
+vim.keymap.set("i", "<C-c>", "<Esc>", { desc = "Exit insert mode" })
+vim.keymap.set("n", "<C-c>", ":nohl<CR>", { desc = "Clear search highlights", silent = true })
+vim.keymap.set("n", "Q", "<nop>", { desc = "Disable Ex mode (unintended triggers)" })
 
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Replace word cursor is on globally" })
+-- LSP & Refactoring
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format current buffer" })
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Search and replace word under cursor" })
 
+-- Autocommands
 vim.api.nvim_create_autocmd("TextYankPost", {
     desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup("kickstart-highlight-yang", { clear = true }),
+    group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
     callback = function()
         vim.highlight.on_yank()
     end,
 })
-
-
