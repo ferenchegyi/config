5aeb1c580070c23d8fc52844c8d95c5790a5156b
Ferenc Hegyi <hegyiferenc@proton.me>
2025-12-25 23:35:43 +0100

    NVIM: Optimize telescope and remove unused pre-built plugins

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,50 +1,50 @@
 return {
-	"nvim-telescope/telescope.nvim",
-    tag = 'v0.2.0',
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-		"nvim-tree/nvim-web-devicons",
-		"andrew-george/telescope-themes",
-	},
-	config = function()
-		local telescope = require("telescope")
-		local actions = require("telescope.actions")
-		local builtin = require("telescope.builtin")
+  "nvim-telescope/telescope.nvim",
+  tag = "v0.2.0",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-tree/nvim-web-devicons",
+    "andrew-george/telescope-themes",
+  },
+  -- Moving keys here allows lazy.nvim to load Telescope only when needed
+  keys = {
+    { "<leader>pr", "<cmd>Telescope oldfiles<CR>", desc = "Fuzzy find recent files" },
+    { "<leader>ths", "<cmd>Telescope themes<CR>", desc = "Theme Switcher" },
+    { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
+    { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Telescope live grep" },
+    { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Telescope buffers" },
+    { "<leader>fh", "<cmd>Telescope help_tags<cr>", desc = "Telescope help tags" },
+  },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
 
-		telescope.load_extension("fzf")
-		telescope.load_extension("themes")
+    telescope.setup({
+      defaults = {
+        path_display = { "smart" },
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-j>"] = actions.move_selection_next,
+            ["<esc>"] = actions.close,
+          },
+        },
+      },
+      extensions = {
+        themes = {
+          enable_previewer = true,
+          enable_live_preview = true,
+          persist = {
+            enabled = true,
+            path = vim.fn.stdpath("config") .. "/lua/colorscheme.lua",
+          },
+        },
+      },
+    })
 
-		telescope.setup({
-			defaults = {
-				path_display = { "smart" },
-				mappings = {
-					i = {
-						["<C-k>"] = actions.move_selection_previous,
-						["<C-j>"] = actions.move_selection_next,
-					},
-				},
-			},
-			extensions = {
-				themes = {
-					enable_previewer = true,
-					enable_live_preview = true,
-					persist = {
-						enabled = true,
-						path = vim.fn.stdpath("config") .. "/lua/colorscheme.lua",
-					},
-				},
-			},
-		})
-
-        -- Keymaps
-        vim.keymap.set("n", "<leader>pr", "<cmd>Telescope oldfiles<CR>", { desc = "Fuzzy find recent files" })
-
-        vim.keymap.set("n", "<leader>ths", "<cmd>Telescope themes<CR>", { noremap = true, silent = true, desc = "Theme Switcher" })
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
-    end,
+    -- Load extensions after setup
+    telescope.load_extension("fzf")
+    telescope.load_extension("themes")
+  end,
 }
